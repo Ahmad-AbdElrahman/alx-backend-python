e6b4e888feeb93697a502f9ecb4cd537d3f71bdc
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-08-01 20:25:19 +0300

    4th task

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+import math
+
+
+def to_str(n: float) -> str:
+    """
+    function that takes a float n as argument
+    and returns the string representation of the float.
+    """
+    return str(n)
