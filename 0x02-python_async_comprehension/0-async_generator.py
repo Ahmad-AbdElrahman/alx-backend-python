f0b19c6bec7bc266536becfcbe0fb8023b0c3c46
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-08-08 02:20:16 +0300

    1st task

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+The basics of async
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0,10)
